074d9c2e3d11761bf73521049cac69ffa7e03907
hulsizer <andrew.hulsizer@gmail.com>
2026-01-06 09:26:22 -0600

    Update to better show problem

--- ResetingView/ContentView.swift
+++ ResetingView/ContentView.swift
@@ -10,60 +10,92 @@ import SwiftUI
 struct ContentView: View {
     @State var displayWithVStack: Bool = false
     @State var displayWithoutVStack: Bool = false
+    @State var useNavigationView: Bool = false
+    
     var body: some View {
         TabView {
             Tab {
                 VStack(spacing: 40) {
-                    Button("With VStack (Doesn't Work)") {
+                    Spacer()
+                    Button("With VStack") {
                         displayWithVStack = true
                     }
-                    Button("No VStack (Works)") {
+                    Button("No VStack") {
                         displayWithoutVStack = true
                     }
+                    Spacer()
+                    Toggle("Use NavigationView", isOn: $useNavigationView)
                 }
+                .padding(.horizontal)
                 .sheet(isPresented: $displayWithVStack) {
-                    NavigationStack {
+                    wrappedNavigationView {
                         VStack {
-                            HackView()
+                            FirstView(expectationToWork: useNavigationView ? true : false)
                         }
                     }
                 }
                 .sheet(isPresented: $displayWithoutVStack) {
-                    NavigationStack {
-                        HackView()
+                    wrappedNavigationView {
+                        FirstView(expectationToWork: true)
                     }
                 }
             }
         }
     }
-}
-
-public struct HackView: View {
     
-    @State var isDisplayed: Bool = false
-    public var body: some View {
-        Button("Quick") {
-            isDisplayed = true
-        }.sheet(isPresented: $isDisplayed) {
-            EditRecipeView()
+    @ViewBuilder
+    func wrappedNavigationView<Content: View>(
+        @ViewBuilder content: () -> Content
+    ) -> some View {
+        if useNavigationView {
+            NavigationView {
+                content()
+            }
+        } else {
+            NavigationStack {
+                content()
+            }
         }
     }
 }
 
-struct EditRecipeView: View {
-    @State var clicked: Bool = false
+public struct FirstView: View {
+    
+    @State var isDisplayed: Bool = false
+    
+    let expectationToWork: Bool
+    
+    public var body: some View {
+        Button("Display Secondary State View") {
+            isDisplayed = true
+        }.sheet(isPresented: $isDisplayed) {
+            SecondaryStateView(expectationToWork: expectationToWork)
+        }
+    }
+}
 
+struct SecondaryStateView: View {
+    @State var isCircle: Bool = false
+    @Namespace var namespace
+    
+    let expectationToWork: Bool
+    
     var body: some View {
-        let _ = Self._printChanges()
-        Color.black
-            .onTapGesture {
-                clicked = true
+        VStack(spacing: 20) {
+            Rectangle()
+                .fill(expectationToWork ? .green : .red)
+                .frame(width: 88, height: 88)
+                .cornerRadius(isCircle ? 44 : 0)
+                .animation(.bouncy, value: isCircle)
+            
+            VStack(alignment: .leading, spacing: 12) {
+                Toggle("1. Make Circle", isOn: $isCircle)
+                Text("2. Background and Foreground the App")
+                Text(expectationToWork ? "3. Shape should maintain" : "3. Shape will reset")
             }
-            .overlay {
-                if clicked {
-                    Color.red
-                }
-            }
+            
+        }
+        .padding(.horizontal)
     }
 }
 
